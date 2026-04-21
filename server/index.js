f3efb4dedc70f419b910dd56f1a472e37e08be15
방정욱/기술기획1그룹(SR)/삼성전자 <jwook.bang@samsung.com>
2026-04-21 09:56:22 +0900

    RSS: Add DigitalTrends to discovery feeds

--- server/index.js
+++ server/index.js
@@ -73,6 +73,7 @@ const RSS_FEEDS = [
   { name: "The Verge", url: "https://www.theverge.com/rss/index.xml" },
   { name: "Wired", url: "https://www.wired.com/feed/category/business/latest/rss" },
   { name: "CNET", url: "https://www.cnet.com/rss/all/" },
+  { name: "DigitalTrends", url: "https://www.digitaltrends.com/feed/" },
   { name: "VentureBeat", url: "https://venturebeat.com/feed" },
   { name: "Techmeme", url: "https://www.techmeme.com/feed.xml" },
   { name: "Hugging Face", url: "https://huggingface.co/blog/feed.xml" },
